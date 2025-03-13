94e993c5bca65f662e27eb0e6ca77d660454f3dc
Chhaya Haribhau Mandlik <krushikirtimandlik@gmail.com>
2025-03-13 19:18:32 +0530

    Update About.jsx

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const About = () => {
+
+const Admin = () => {
   return (
     <div>
-        <h2>About</h2>
+      <h2>ADMIN LOGIN PAGE</h2>
+      <div className='bg-warning w-50 mx-auto p-4 text-center mt-5 rounded-4'>
+          <input type='text' placeholder='Enter Username' className='form-control mb-3 p-3'/>
+          <input type='password' placeholder='Enter Password' className='form-control mb-3 p-3'/>
+          <button className='btn btn-success w-25'>LOGIN</button>
+      </div>
     </div>
   )
 }
 
-export default About
+
+export default Admin
+
