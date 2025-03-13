5e031463bfe7bf1e1f264f94eb429ffb927b6c05
Chhaya Haribhau Mandlik <krushikirtimandlik@gmail.com>
2025-03-13 19:17:51 +0530

    Update Home.jsx

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,11 +1,13 @@
 
-import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
+import React from 'react'
+import BannerImage from './banner-image.png'
+export default function Home() {
+
+  return (document.title = "Student Management System",
+    <div className='text-center'>
+        <h1 className='text-center'>Student Management System</h1>
+        <img src={BannerImage} alt='banner' className='logo img-fluid'/>
+    </div>
   )
 }
-
-export default Home
-
